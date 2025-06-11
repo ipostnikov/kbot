2de1278ccb40048868d6b79e6c735f7cd8b884dd
Ihor <i.postnikov97@gmail.com>
2025-06-11 22:25:53 +0000

    upd pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,26 +1,51 @@
 pipeline {
     agent any
-    
-    parameters {
-        choice(
-            name: 'OS',
-            choices: ['linux', 'darwin', 'windows'],
-            description: 'Target operating system'
-        )
-        choice(
-            name: 'ARCH',
-            choices: ['amd64', 'arm64'],
-            description: 'Target architecture'
-        )
-        booleanParam(
-            name: 'SKIP_TESTS',
-            defaultValue: false,
-            description: 'Skip running tests'
-        )
-        booleanParam(
-            name: 'SKIP_LINT',
-            defaultValue: false,
-            description: 'Skip running linter'
-        )
+
+    environment {
+        REPO = 'https://github.com/ipostnikov/kbot'
+        BRANCH = 'main'
+    }
+
+    stages {
+        stage("clone") {
+            steps {
+                echo 'CLONE REPOSITORY'
+                git branch: "${BRANCH}", url: "${REPO}"
+            }
+        }
+
+        stage("test") {
+            steps {
+                echo 'TEST EXECUTION STARTED'
+                sh 'make test'
+            }
+        }
+
+        stage("build") {
+            steps {
+                echo 'BUILD EXECUTION STARTED'
+                sh 'make build'
+            }
+        }
+
+        stage("image") {
+            steps {
+                script {
+                    echo 'BUILD EXECUTION STARTED'
+                    sh 'make image'
+                }
+            }
+        }
+
+        stage("push") {
+            steps {
+                script {
+                    // Changed Docker registry to ghcr.io
+                    docker.withRegistry('ghcr.io') { 
+                        sh 'make push'
+                    }
+                }
+            }
+        }
     }
 }
